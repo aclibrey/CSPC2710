e0087ae0d1241540faf429dea2b97b9895745da1
aclibrey <142695183+aclibrey@users.noreply.github.com>
2023-08-22 19:44:12 -0500

    Update SeatReservation.java

---
+++ SeatReservation.java
@@ -0,0 +1,55 @@
+import java.time.LocalDate;
+
+public class SeatReservation {
+
+  private String flightDesignator;
+  private java.time.LocalDate flightDate;
+  private String firstName;
+  private String lastName;
+
+  public String getFlightDesignator(){
+    return flightDesignator;  
+  }
+
+  public void setFlightDesignator(String fd) {
+    if(fd.length() < 4 || fd.length() > 6) {
+      throw new IllegalArgumentException("Invalid FlightDesignator Length");
+    }
+    this.flightDesignator = fd;
+  }
+
+  public LocalDate getFlightDate() {
+    return flightDate;
+  }
+
+  public void setFlightDate(LocalDate date) {
+    this.flightDate = date;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String fn) {
+    this.firstName = fn;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String ln) {
+    this.lastName = ln;
+  }
+
+  @Override
+  public String toString() {
+    return "SeatReservation{" + 
+      "flightDesignator=" + (flightDesignator == null ? "null" : flightDesignator) + 
+      ", flightDate=" + (flightDate == null ? "null" : flightDate) + 
+      ", firstName=" + (firstName == null ? "null" : firstName) +
+      ", lastName=" + (lastName == null ? "null" : lastName) +
+      '}';
+  }
+  
+}
